0d768d3d3f3cc7224e3f18228c65dafc0f61641a
M.Farooq Azaad <mr.farooq2066@gmail.com>
2023-04-29 19:13:42 +0500

    Weather app

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { WeatherComponent } from './weather/weather.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
